c4899997315104a915cdb35ca1a72c476dcf34e0
xwang <xunwang@wenba100.com>
2017-07-28 17:04:31 +0800

    mediaprojection

--- otherapp/src/main/java/com/xun/mpd/otherapp/OtherAppMainActivity.java
+++ otherapp/src/main/java/com/xun/mpd/otherapp/OtherAppMainActivity.java
@@ -4,29 +4,43 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.xun.mpd.IMyAidlInterface;
 import com.xun.mpd.commlib.appconn.manager.MessengerConnManager;
 import com.xun.mpd.commlib.util.AppUtil;
 import com.xun.mpd.otherapp.capture.ScreenCaptureActivity;
+import com.xun.mpd.otherapp.mediaprojection.CaptureManager;
+import com.xun.mpd.otherapp.mediaprojection.ScreenRecorder;
+
+import java.io.File;
 
 public class OtherAppMainActivity extends AppCompatActivity {
-    private Button aidlBtn, messengerBtn;
+    public static final int REQUEST_MEDIA_PROJECTION = 18;
+    private static final int REQUEST_CODE = 19;
+    private MediaProjectionManager mMediaProjectionManager;
+    private Button aidlBtn, messengerBtn, recordBtn, captureBtn;
     private boolean isConn;
     private IMyAidlInterface mIMyAidlInterface;
     private Button screen_capture_btn;
+    private ScreenRecorder mRecorder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_app_main);
+        mMediaProjectionManager = (MediaProjectionManager) getSystemService(MEDIA_PROJECTION_SERVICE);
 
         aidlBtn = (Button) findViewById(R.id.aidl_btn);
         aidlBtn.setOnClickListener(new View.OnClickListener() {
@@ -64,6 +78,39 @@ public class OtherAppMainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        recordBtn = (Button) findViewById(R.id.recorder);
+        recordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mRecorder != null) {
+                    mRecorder.quit();
+                    mRecorder = null;
+                    recordBtn.setText("Restart recorder");
+                } else {
+                    Intent captureIntent = mMediaProjectionManager.createScreenCaptureIntent();
+                    startActivityForResult(captureIntent, REQUEST_CODE);
+                }
+            }
+        });
+        captureBtn = (Button) findViewById(R.id.capture);
+        captureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestCapturePermission();
+            }
+        });
+    }
+
+    public void requestCapturePermission() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            //5.0 之后才允许使用屏幕截图
+            return;
+        }
+        MediaProjectionManager mediaProjectionManager = (MediaProjectionManager)
+                getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+        startActivityForResult(
+                mediaProjectionManager.createScreenCaptureIntent(),
+                REQUEST_MEDIA_PROJECTION);
     }
 
     ServiceConnection mConnection = new ServiceConnection() {
@@ -85,4 +132,46 @@ public class OtherAppMainActivity extends AppCompatActivity {
             isConn = false;
         }
     };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_MEDIA_PROJECTION:
+                if (resultCode == RESULT_OK && data != null) {
+                    CaptureManager.setResultData(data);
+                    CaptureManager.getInstance(getApplicationContext()).startScreenShot();
+                }
+                break;
+            case REQUEST_CODE:
+                MediaProjection mediaProjection = mMediaProjectionManager.getMediaProjection(resultCode, data);
+                if (mediaProjection == null) {
+                    Log.e("@@", "media projection is null");
+                    return;
+                }
+                // video size
+                final int width = 1280;
+                final int height = 720;
+                File file = new File(Environment.getExternalStorageDirectory(),
+                        "record-" + width + "x" + height + "-" + System.currentTimeMillis() + ".mp4");
+                final int bitrate = 6000000;
+                mRecorder = new ScreenRecorder(width, height, bitrate, 1, mediaProjection, file.getAbsolutePath());
+                mRecorder.start();
+                recordBtn.setText("Stop Recorder");
+                Toast.makeText(this, "Screen recorder is running...", Toast.LENGTH_SHORT).show();
+                moveTaskToBack(true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mRecorder != null){
+            mRecorder.quit();
+            mRecorder = null;
+        }
+    }
 }
---
+++ otherapp/src/main/java/com/xun/mpd/otherapp/mediaprojection/CaptureManager.java
@@ -0,0 +1,201 @@
+package com.xun.mpd.otherapp.mediaprojection;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.PixelFormat;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.Image;
+import android.media.ImageReader;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.v4.os.AsyncTaskCompat;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.WindowManager;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+
+/**
+ * Created by xunwang on 2017/7/28.
+ */
+
+public class CaptureManager {
+    private static CaptureManager instance;
+    private Context mApplicationContext;
+    private MediaProjection mMediaProjection;
+    private VirtualDisplay mVirtualDisplay;
+    private ImageReader mImageReader;
+    private static Intent mResultData = null;
+
+    private int mScreenWidth;
+    private int mScreenHeight;
+    private int mScreenDensity;
+
+    private CaptureManager(Context context) {
+        mApplicationContext = context;
+        DisplayMetrics metrics = new DisplayMetrics();
+        WindowManager mWindowManager = (WindowManager) mApplicationContext.getSystemService(Context.WINDOW_SERVICE);
+        mWindowManager.getDefaultDisplay().getMetrics(metrics);
+        mScreenDensity = metrics.densityDpi;
+        mScreenWidth = metrics.widthPixels;
+        mScreenHeight = metrics.heightPixels;
+        createImageReader();
+    }
+
+    public static CaptureManager getInstance(Context context) {
+        if (instance == null) {
+            synchronized (CaptureManager.class) {
+                if (instance == null) {
+                    instance = new CaptureManager(context);
+                }
+            }
+        }
+        return instance;
+    }
+
+
+    public void startScreenShot() {
+        Log.d("kkkkkkkk", "startScreenShot");
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                //start virtual
+                startVirtual();
+            }
+        }, 5);
+
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                //capture the screen
+                startCapture();
+            }
+        }, 30);
+    }
+
+    private void startVirtual() {
+        Log.d("kkkkkkkk", "startVirtual mMediaProjection == null? " + (mMediaProjection == null));
+        if (mMediaProjection != null) {
+            virtualDisplay();
+        } else {
+            setUpMediaProjection();
+            virtualDisplay();
+        }
+    }
+
+    public static void setResultData(Intent mResultData) {
+        CaptureManager.mResultData = mResultData;
+    }
+
+    private void createImageReader() {
+        mImageReader = ImageReader.newInstance(mScreenWidth, mScreenHeight, PixelFormat.RGBA_8888, 1);
+    }
+
+    private void startCapture() {
+        Log.d("kkkkkkkk", "startCapture");
+        Image image = mImageReader.acquireLatestImage();
+        if (image == null) {
+            startScreenShot();
+        } else {
+            SaveTask mSaveTask = new SaveTask();
+            AsyncTaskCompat.executeParallel(mSaveTask, image);
+        }
+    }
+
+    private void virtualDisplay() {
+        Log.d("kkkkkkkk", "virtualDisplay");
+        mVirtualDisplay = mMediaProjection.createVirtualDisplay("screen-mirror",
+                mScreenWidth, mScreenHeight, mScreenDensity, DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+                mImageReader.getSurface(), null, null);
+    }
+
+    private void setUpMediaProjection() {
+        if (mResultData == null) {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            mApplicationContext.startActivity(intent);
+        } else {
+            mMediaProjection = getMediaProjectionManager().getMediaProjection(Activity.RESULT_OK, mResultData);
+        }
+    }
+
+    private MediaProjectionManager getMediaProjectionManager() {
+        return (MediaProjectionManager) mApplicationContext.getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+    }
+
+    private class SaveTask extends AsyncTask<Image, Void, String> {
+        @Override
+        protected String doInBackground(Image... params) {
+            if (params == null || params.length < 1 || params[0] == null) {
+                return null;
+            }
+
+            Image image = params[0];
+
+            int width = image.getWidth();
+            int height = image.getHeight();
+            final Image.Plane[] planes = image.getPlanes();
+            final ByteBuffer buffer = planes[0].getBuffer();
+            //每个像素的间距
+            int pixelStride = planes[0].getPixelStride();
+            //总的间距
+            int rowStride = planes[0].getRowStride();
+            int rowPadding = rowStride - pixelStride * width;
+            Bitmap bitmap = Bitmap.createBitmap(width + rowPadding / pixelStride, height, Bitmap.Config.ARGB_8888);
+            bitmap.copyPixelsFromBuffer(buffer);
+            bitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height);
+            image.close();
+            File fileImage = null;
+            if (bitmap != null) {
+                try {
+                    fileImage = new File(FileUtil.getScreenShotsName(mApplicationContext));
+                    if (!fileImage.exists()) {
+                        fileImage.createNewFile();
+                    }
+                    FileOutputStream out = new FileOutputStream(fileImage);
+                    if (out != null) {
+                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+                        out.flush();
+                        out.close();
+                        Intent media = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                        Uri contentUri = Uri.fromFile(fileImage);
+                        media.setData(contentUri);
+                        mApplicationContext.sendBroadcast(media);
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    fileImage = null;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    fileImage = null;
+                }
+            }
+
+            if (fileImage != null) {
+                return fileImage.getAbsolutePath();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String ImgPath) {
+            super.onPostExecute(ImgPath);
+            //预览图片
+            if (ImgPath != null) {
+                //在华为PAD上会出现为空的情况，如果是空取本地的
+                Log.d("kkkkkkkk", "截屏图片保存成功 ImgPath --> " + ImgPath);
+            }
+        }
+    }
+}
